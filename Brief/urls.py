01e60ceee3a5e648d0e2ce73d869c98d16299e0b
SajjadSj66 <147406621+SajjadSj66@users.noreply.github.com>
2025-01-23 23:49:25 +0330

    Update urls.py

--- Brief/urls.py
+++ Brief/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/',include('briefmarket.urls'))
+    path('',include('briefmarket.urls'))
 ]
